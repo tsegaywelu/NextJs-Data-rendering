13ce1ecc258ba5173b53ba8922cabe5053b43c70
tsegay <tsegaygebrezgi56@gmail.com>
2025-09-04 17:38:15 +0300

    .gitigonre is updated now

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { getFeaturedEvents } from '../helpers/api-util';
-import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
 
 function HomePage(props) {
   return (
@@ -8,16 +8,21 @@ function HomePage(props) {
     </div>
   );
 }
+//this function will run at build time on the server side
+//this function will never run on the client side
+//this function will be used to fetch data from an API or database
+//this function will be used to pre-render the page with the fetched data
+//this function will be used to revalidate the page every 30 minutes
 
 export async function getStaticProps() {
   const featuredEvents = await getFeaturedEvents();
 
   return {
     props: {
-      events: featuredEvents
+      events: featuredEvents,
     },
-    revalidate: 1800
-  }
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
