84dcc7254371aacf0f5f0ce8e0bd05aba4ed3a02
tsegay <tsegaygebrezgi56@gmail.com>
2025-09-05 12:21:01 +0300

    check this one

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      //this is is optional on the current version of next js
     </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import { Fragment } from "react";
+
+const headdata = (
+  <head>
+    <title>NextJS Events</title>
+    <meta
+      name="description"
+      content="Find a lot of great events that allow you to evolve..."
+    />
+  </head>
+);
 
 function HomePage(props) {
   return (
-    <div>
-      <EventList items={props.events} />
-    </div>
+    <>
+      {headdata}
+
+      <div>
+        <EventList items={props.events} />
+      </div>
+    </>
   );
 }
 //let's solve this very importnat issue of fetching data from an external API
